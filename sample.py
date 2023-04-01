5f0c16449ab7c299b96ff8f52419394104c2f761
gomesuit <9404411+gomesuit@users.noreply.github.com>
2023-04-01 23:16:43 +0900

    save

--- sample.py
+++ sample.py
@@ -6,7 +6,7 @@ model_id = "stabilityai/stable-diffusion-2-1"
 # Use the DPMSolverMultistepScheduler (DPM-Solver++) scheduler here instead
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
 pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
-pipe = pipe.to("cuda")
+pipe = pipe.to("mps")
 
 prompt = "a photo of an astronaut riding a horse on mars"
 image = pipe(prompt).images[0]
