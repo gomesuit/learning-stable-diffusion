fe5970a778f164433c77772c46a5fac56c5c8702
gomesuit <9404411+gomesuit@users.noreply.github.com>
2023-04-03 00:04:53 +0900

    save

---
+++ sample_2.py
@@ -0,0 +1,14 @@
+import torch
+from torch import autocast
+from diffusers import StableDiffusionPipeline
+
+pipe = StableDiffusionPipeline.from_pretrained(
+    'hakurei/waifu-diffusion',
+    torch_dtype=torch.float32
+).to('mps')
+
+prompt = "1girl, aqua eyes, baseball cap, blonde hair, closed mouth, earrings, green background, hat, hoop earrings, jewelry, looking at viewer, shirt, short hair, simple background, solo, upper body, yellow shirt"
+
+image = pipe(prompt, guidance_scale=6).images[0]
+
+image.save("test.png")
