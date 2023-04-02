23563ae5411a3bbb4e650f7a113d5eeed7873a49
gomesuit <9404411+gomesuit@users.noreply.github.com>
2023-04-02 11:34:31 +0900

    save

--- sample.py
+++ sample.py
@@ -3,8 +3,12 @@ from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
 
 model_id = "stabilityai/stable-diffusion-2-1"
 
-# Use the DPMSolverMultistepScheduler (DPM-Solver++) scheduler here instead
-pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+pipe = StableDiffusionPipeline.from_pretrained(
+    model_id,
+    revision="fp16",
+    torch_dtype=torch.float16,
+)
+
 pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
 pipe = pipe.to("mps")
 
