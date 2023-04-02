12bbda77183f066f63dca1214b594b5a2aff9dce
gomesuit <9404411+gomesuit@users.noreply.github.com>
2023-04-02 11:40:27 +0900

    save

--- sample.py
+++ sample.py
@@ -5,8 +5,7 @@ model_id = "stabilityai/stable-diffusion-2-1"
 
 pipe = StableDiffusionPipeline.from_pretrained(
     model_id,
-    revision="fp16",
-    torch_dtype=torch.float16,
+    torch_dtype=torch.float,
 )
 
 pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
